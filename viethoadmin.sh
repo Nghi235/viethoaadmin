01e4560d55a11ac79416cc1695eaea2b825d4d5d
nghi235 <101364463+Nghi235@users.noreply.github.com>
2022-06-10 17:05:26 +0700

    Update viethoadmin.sh

--- viethoadmin.sh
+++ viethoadmin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf runvhadmin.x
-cd /www/wwwroot/hvpn.xyz/public/assets/admin
+cd /www/wwwroot/maxdata4g.com/public/assets/admin
 rm -rf umi.js
 curl -OL https://raw.githubusercontent.com/Nghi235/viethoaadmin/main/umi.js
 cd ../../../../../..
